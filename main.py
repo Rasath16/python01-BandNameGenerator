d669193d24e5e70032fbbe2fc283b93587aa4f48
TharushaRasath <23322043-TharushaRasath@users.noreply.replit.com>
2024-01-13 15:52:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print ("Answer the following questions then you will have your band name in seconds")
+music_genre = input("What is your favourite music genre?\n")
+fav_animal = input("Who is your favourite animal ?\n")
+print ("Your band name is " + music_genre + " " + fav_animal)
